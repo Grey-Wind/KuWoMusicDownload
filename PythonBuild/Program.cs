dd8f876e991016c3a617bb80ffadbb4d33cb2be0
Grey Wind <98391767+Grey-Wind@users.noreply.github.com>
2023-04-21 19:53:57 +0800

    创建python快速构建器

---
+++ PythonBuild/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+
+namespace PythonBuild
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string command = "pyinstaller -F -i icon.png main.py --noconsole"; // 替换成需要执行的命令
+
+            ExecuteCommand(command);
+        }
+
+        static void ExecuteCommand(string command)
+        {
+            var processInfo = new ProcessStartInfo()
+            {
+                FileName = "cmd.exe",
+                Arguments = $"/C {command}",
+                CreateNoWindow = true,
+                UseShellExecute = false,
+                RedirectStandardOutput = true
+            };
+
+            using (var process = new Process())
+            {
+                process.StartInfo = processInfo;
+                process.OutputDataReceived += (sender, e) => Console.WriteLine(e.Data);
+                process.Start();
+                process.BeginOutputReadLine();
+                process.WaitForExit();
+            }
+        }
+    }
+}
